b84d852db015954ae503a0fd09ea95c09301f189
Kelvin Dart <kelvin.dart@crimsontide.co.uk>
2016-10-10 23:26:59 +0100

    Added a simple timer

--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+        var parentElement   = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
@@ -45,6 +45,16 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+        this.setupTimer();
+    },
+    setupTimer: function () {
+        var timerElement = document.getElementById('timer');
+        var time = 1;
+
+        setInterval(function () {
+            timerElement.innerHTML = time++;
+        }, 1000);
     }
 };
 
